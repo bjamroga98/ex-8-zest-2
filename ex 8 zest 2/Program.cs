1faa667c7996d3fd01ceeb9a3b30bc59787e4944
barbara.jamroga <barbara.jamroga@microsoft.wsei.edu.pl>
2019-04-11 08:34:49 +0200

    commit

--- ex 8 zest 2/Program.cs
+++ ex 8 zest 2/Program.cs
@@ -10,6 +10,20 @@ namespace ex_8_zest_2
     {
         static void Main(string[] args)
         {
+            // Definitions
+            int a = 5;
+            int b = ++a;
+            int c = a++;
+            int f = 15;
+            int g = --f;
+            int h = f--;
+
+            //Printing
+            Console.WriteLine("{0} {1} {2}", a, b, c);
+            Console.WriteLine("{0} {1} {2}", f, g, h);
+
+            Console.ReadKey();
+
         }
     }
 }
